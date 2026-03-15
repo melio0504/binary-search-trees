f6b1edfb3e7a4c368b02760a8f2675c7485c940f
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-15 13:33:25 +0800

    Create Tree class and its empty method for now

---
+++ Tree.js
@@ -0,0 +1,55 @@
+import Node from './Node.js';
+
+export default class Tree {
+  constructor(arr) {
+    this.root = null;
+  }
+
+  buildTree(arr) {
+
+  }
+
+  includes(value) {
+
+  }
+
+  insert() {
+
+  }
+
+  deleteItem(value) {
+
+  }
+
+  levelOrderForEach(callback) {
+
+  }
+
+  inOrderForEach(callback) {
+
+  }
+
+  preOrderForEach(callback) {
+
+  }
+
+  postOrderForEach(callback) {
+
+  }
+
+  height(value) {
+
+  }
+
+  depth(value) {
+
+  }
+
+  isBalanced() {
+
+  }
+
+  rebalance() {
+
+  }
+}
