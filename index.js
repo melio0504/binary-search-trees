f9466ecd1f1526e48454f59dcd58714608125c14
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-19 20:39:49 +0800

    Implement Binary Search Tree in terminal
    This is fun btw!

--- index.js
+++ index.js
@@ -22,3 +22,35 @@ const printTraversal = (tree, label, traversalMethod) => {
 
 const initialValues = randomArray();
 const tree = new Tree(initialValues);
+
+console.log('Initial random values:', initialValues.join(', '));
+console.log('Tree is balanced:', tree.isBalanced());
+console.log('\nInitial tree:');
+prettyPrint(tree.root);
+
+console.log('\nTraversals (balanced tree):');
+printTraversal(tree, 'Level order', tree.levelOrderForEach);
+printTraversal(tree, 'Pre order', tree.preOrderForEach);
+printTraversal(tree, 'Post order', tree.postOrderForEach);
+printTraversal(tree, 'In order', tree.inOrderForEach);
+
+const unbalancingValues = [101, 150, 200, 250, 300, 350, 400];
+unbalancingValues.forEach((value) => tree.insert(value));
+
+console.log('\nAdded values > 100:', unbalancingValues.join(', '));
+console.log('Tree is balanced:', tree.isBalanced());
+console.log('\nUnbalanced tree:');
+prettyPrint(tree.root);
+
+tree.rebalance();
+
+console.log('\nTree was rebalanced.');
+console.log('Tree is balanced:', tree.isBalanced());
+console.log('\nRebalanced tree:');
+prettyPrint(tree.root);
+
+console.log('\nTraversals (rebalanced tree):');
+printTraversal(tree, 'Level order', tree.levelOrderForEach);
+printTraversal(tree, 'Pre order', tree.preOrderForEach);
+printTraversal(tree, 'Post order', tree.postOrderForEach);
+printTraversal(tree, 'In order', tree.inOrderForEach);
